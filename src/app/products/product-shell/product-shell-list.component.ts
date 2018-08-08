f7126a001517976dd479bb9bdd603a4858c389af
dinesg <dinesh.k.g@nokia.com>
2018-08-08 16:48:10 +0530

    Ch9. Communication through Service Notification

--- src/app/products/product-shell/product-shell-list.component.ts
+++ src/app/products/product-shell/product-shell-list.component.ts
@@ -1,28 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { IProduct } from '../product';
 import { ProductService } from '../product.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'pm-product-shell-list',
   templateUrl: './product-shell-list.component.html'
 })
-export class ProductShellListComponent implements OnInit {
+export class ProductShellListComponent implements OnInit, OnDestroy {
+
   pageTitle: string = 'Products';
   errorMessage: string;
   products: IProduct[];
+  selectedProduct: IProduct;
+
+  private productsSub: Subscription;
+  private selProductSub: Subscription;
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.productService.getProducts().subscribe(
+    this.productsSub = this.productService.getProducts().subscribe(
       (products: IProduct[]) => {
         this.products = products;
       },
       (error: any) => this.errorMessage = <any>error
     );
+
+    this.selProductSub = this.productService.selectedProductChanges$.subscribe( 
+      selectedProduct => this.selectedProduct = selectedProduct 
+    );
   }
+  ngOnDestroy(): void {
+    this.productsSub.unsubscribe();
+    this.selProductSub.unsubscribe();
+  }
+
   onSelected(product: IProduct): void {
-    this.productService.currentProduct = product;
+    this.productService.changeSelectedProduct(product);
   }
 }
--- src/app/products/product-shell/product-shell.component.ts
+++ src/app/products/product-shell/product-shell.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     templateUrl: './product-shell.component.html'
 })
-export class ProductShellComponent implements OnInit {
+export class ProductShellComponent implements OnInit, OnDestroy {
     pageTitle: string = 'Products';
     monthCount: number;
+    private selProductSub: Subscription;
 
-    constructor() { }
+    constructor(private productService: ProductService) { }
 
     ngOnInit() {
+        this.selProductSub = this.productService.selectedProductChanges$.subscribe( 
+            selectedProduct => {
+                if(selectedProduct){
+                    const start = new Date( selectedProduct.releaseDate );
+                    const end = new Date();
+                    this.monthCount = end.getMonth() - start.getMonth() 
+                            + ( 12 * (end.getFullYear() - start.getFullYear() ) );
+                } else {
+                    this.monthCount = 0;
+                }
+            }
+        );
+    }
+
+    ngOnDestroy(): void {
+        this.selProductSub.unsubscribe();
     }
 
 }
